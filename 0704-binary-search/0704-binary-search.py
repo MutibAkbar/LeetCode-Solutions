7480b5e240eb3b34da6bef03c62ca7cbd1b0ee7a
Mutiba Akbar <48567444+MutibAkbar@users.noreply.github.com>
2023-06-23 19:26:45 +0500

    Time: 243 ms (73.59%), Space: 17.8 MB (90.20%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,5 +1,5 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
 
